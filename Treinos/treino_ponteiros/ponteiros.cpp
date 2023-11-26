3bdc0b017e3d8acf2dec158ac8b4911c7380f93a
Erick Eduardo <erickess2002@gmail.com>
2023-11-26 14:47:25 -0300

    commit-treino_poo

---
+++ Treinos/treino_ponteiros/ponteiros.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //////////////Passagem por valor e por referencia////////////////////////////
+
+
+// int somaValor(int a, int b, int c){
+
+//   c = a + b;
+
+//   return c;
+
+// }
+
+// int somaReferencia(int a, int b, int &c){
+
+//   c = a + b;
+//   // Posso passar todos os parametros por ref ou somente o que quero alterar mesmo
+
+// }
+
+  // int a = 0, b = 0, c = 0;
+
+  // a = 1;
+  // b = 4;
+
+  // std::cout << a << "\n" << b << "\n" << c << "\n";
+
+  // std::cout << "Soma por valor: " << somaValor(a, b, c) << " --- Valor de c: " << c << "\n";
+  // somaReferencia(a, b,  c);
+  // std::cout << "Soma por referencia: --- " << "Valor de c: " << c;
+
+
+
+
+
+  ////////////////////Ponteiro de Ponteiro////////////////////////////
+  // int a = 50;
+  // int *ptr_a = nullptr;
+  // int **ptr_ptr_a = nullptr;
+
+  // ptr_a = &a;
+  // ptr_ptr_a = &ptr_a;
+
+  // std::cout << "Valor de a = " << a << "\n" << "Endereco de a: " << &a << "\n";
+  // std::cout << "Endereco de ptr_a = "  << &ptr_a << "/// Valor apontado por ptr_a: " << *ptr_a  << "\n" ;
+  // std::cout << "Endereco de ptr_ptr_a = " << &ptr_ptr_a << "\n" << "Valor (endereco de ptr_a) apontado por ptr_ptr_a: " << *ptr_ptr_a << "\n" << "Valor apontado pelo ponteiro apontado por ptr_ptr_a: " << **ptr_ptr_a;
+
+  //   return 0;
+  
+
+}
