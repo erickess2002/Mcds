6267ea54c1818f6be9a586eef733ab17ed0775b4
Erick Eduardo <erickess2002@gmail.com>
2023-12-16 01:32:19 -0300

    commit - consertando layout da calc e colocando calculator em funções construtoras

--- JavaScript/calculator/assets/javascript/main.js
+++ JavaScript/calculator/assets/javascript/main.js
@@ -1,93 +1,184 @@
-function calculator(){
-    return{
-        ////////////// Atributos ////////////////////
+function Calculator(){
 
-        display: document.querySelector('.display'),
+     this.display = document.querySelector('.display');
 
-        /////////////// Metodos ///////////////////
+    this.start = () => {
 
-        start(){
-            alert('Calculadora iniciada!');
-            this.clickBotao();
-            this.enter();
-        },
+        alert('Calculadora iniciada!');
+        this.clickBotao();
+        this.enter();
 
-        enter(){
+    };
 
-            document.addEventListener('keydown', (e) => {
-                if(e.key === "Enter") {
-                    e.preventDefault();
-                    this.display.focus();
-                    this.igual();
-                }
-            });
+    this.enter = () => {
 
-        },
+        document.addEventListener('keydown', (e) => {
+            if(e.key === "Enter") {
+                e.preventDefault();
+                this.display.focus();
+                this.igual();
+            }
+        });
 
-        clickBotao(){
+    };
 
-            document.addEventListener('click', (e) => {
+    this.clickBotao = () => {
 
-                const el = e.target;
+        document.addEventListener('click', (e) => {
 
-                if(el.classList.contains('botao_num')){
-                    this.botaoParaDisplay(el.innerText);
-                    this.display.focus();
-                }
+            const el = e.target;
 
-                if(el.classList.contains('botao_clear')){
-                    this.clearDisplay();
-                    this.display.focus();
-                }
+            if(el.classList.contains('botao_num')){
+                this.botaoParaDisplay(el);
+                this.display.focus();
+            }
 
-                if(el.classList.contains('botao_apagar')){
-                    this.apagaUm();
-                    this.display.focus();
-                }
-                
-                if(el.classList.contains('botao_igual')){
-                    this.igual();
-                    this.display.focus();
-                }
+            if(el.classList.contains('botao_clear')){
+                this.clearDisplay();
+                this.display.focus();
+            }
 
-            });
+            if(el.classList.contains('botao_apagar')){
+                this.apagaUm();
+                this.display.focus();
+            }
+            
+            if(el.classList.contains('botao_igual')){
+                this.igual();
+                this.display.focus();
+            }
 
-        },
+        });
 
-        botaoParaDisplay(val){
-            this.display.value += val;
-        },
+    };
 
-        clearDisplay(){
-            this.display.value = '';
-        },
+    this.botaoParaDisplay = val => {
+        this.display.value += val.innerText;
+    };
 
-        apagaUm(){
-            this.display.value = this.display.value.slice(0, -1);
-        },
+    this.clearDisplay = () => {
+        this.display.value = '';
+    };
 
-        igual(){
-            let valores = this.display.value;
+    this.apagaUm = () => {
+        this.display.value = this.display.value.slice(0, -1);
+    };
 
-            try{
+    this.igual = () => {
+        let valores = this.display.value;
 
-                valores = eval(valores);
+        try{
 
-                if(!valores){
-                    alert('Não é possível realizar a operção, revise o input!');
-                    return;
-                }
+            valores = eval(valores);
 
-                this.display.value = valores;
-
-            }catch(e){
+            if(!valores){
                 alert('Não é possível realizar a operção, revise o input!');
                 return;
             }
-        }
 
+            this.display.value = valores;
+
+        }catch(e){
+            alert('Não é possível realizar a operção, revise o input!');
+            return;
+        }
     };
+
+
 }
 
-const calc = calculator();
+// function calculator(){
+//     return{
+//         ////////////// Atributos ////////////////////
+
+//         display: document.querySelector('.display'),
+
+//         /////////////// Metodos ///////////////////
+
+//         start(){
+
+//             alert('Calculadora iniciada!');
+//             this.clickBotao();
+//             this.enter();
+
+//         },
+
+//         enter(){
+
+//             document.addEventListener('keydown', (e) => {
+//                 if(e.key === "Enter") {
+//                     e.preventDefault();
+//                     this.display.focus();
+//                     this.igual();
+//                 }
+//             });
+
+//         },
+
+//         clickBotao(){
+
+//             document.addEventListener('click', (e) => {
+
+//                 const el = e.target;
+
+//                 if(el.classList.contains('botao_num')){
+//                     this.botaoParaDisplay(el.innerText);
+//                     this.display.focus();
+//                 }
+
+//                 if(el.classList.contains('botao_clear')){
+//                     this.clearDisplay();
+//                     this.display.focus();
+//                 }
+
+//                 if(el.classList.contains('botao_apagar')){
+//                     this.apagaUm();
+//                     this.display.focus();
+//                 }
+                
+//                 if(el.classList.contains('botao_igual')){
+//                     this.igual();
+//                     this.display.focus();
+//                 }
+
+//             });
+
+//         },
+
+//         botaoParaDisplay(val){
+//             this.display.value += val;
+//         },
+
+//         clearDisplay(){
+//             this.display.value = '';
+//         },
+
+//         apagaUm(){
+//             this.display.value = this.display.value.slice(0, -1);
+//         },
+
+//         igual(){
+//             let valores = this.display.value;
+
+//             try{
+
+//                 valores = eval(valores);
+
+//                 if(!valores){
+//                     alert('Não é possível realizar a operção, revise o input!');
+//                     return;
+//                 }
+
+//                 this.display.value = valores;
+
+//             }catch(e){
+//                 alert('Não é possível realizar a operção, revise o input!');
+//                 return;
+//             }
+//         }
+
+//     };
+// }
+
+const calc = new Calculator();
 calc.start();
