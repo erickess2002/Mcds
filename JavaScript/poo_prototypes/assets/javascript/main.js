23a857cf03b6d4d4e7b673fe7ba31aac9c19f0cb
Erick Eduardo <erickess2002@gmail.com>
2023-12-23 17:13:41 -0300

    commit - get e set

--- JavaScript/poo_prototypes/assets/javascript/main.js
+++ JavaScript/poo_prototypes/assets/javascript/main.js
@@ -1,7 +1,37 @@
 function Produto (nome, preco, estoque) {
 
-    this.nome = nome;
-    this.preco = preco;
+    let precoPadrao = preco;
+
+    Object.defineProperties(this, {
+
+        nome:{
+            // get: function(){
+            //     return nome;
+            // },
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: false
+        },
+        
+        preco:{
+            enumerable: true,
+            // value: preco,
+            get: function(){
+                return precoPadrao;
+            },
+            set: function(valor) {
+
+                if(typeof valor !== 'number')
+                    throw new TypeError('Invalid type of argument.')
+                precoPadrao = valor;
+        
+            },
+            // writable: true,
+            configurable: true
+        }
+
+    });
 
     Object.defineProperty(this, 'estoque', {
     
@@ -11,8 +41,21 @@ function Produto (nome, preco, estoque) {
         configurable: false
 
     });
+
+
 }
 
-const prod1 = new Produto('Camisa', 20, 3);
-console.log(prod1);
-console.log(Object.keys(prod1));
+// const prod1 = new Produto('Camisa', 20, 3);
+// console.log(prod1);
+// console.log(Object.keys(prod1));
+
+// delete prod1.preco;
+
+// console.log(prod1);
+// console.log(Object.keys(prod1));
+
+const prod2 = new Produto('Sapato', 0, 0);
+
+prod2.preco = 3123;
+
+console.log(prod2.preco);
